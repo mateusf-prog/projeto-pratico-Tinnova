917e666bc9f79500491cd4b576c17765714b7e4d
mateusfprog <mateus102006@hotmail.com>
2024-06-16 19:43:33 -0300

    created removeProductByName, getProducts and getProductByName methods on the StoreService

--- src/service/StoreService.java
+++ src/service/StoreService.java
@@ -31,6 +31,41 @@ public class StoreService {
         return product;
     }
 
+    public void removeProductByName(String name) {
+
+        if (!isValidateProductName(name)) {
+            throw new InvalidProducDataException("Nome do produto não pode ser nulo ou vazio");
+        }
+
+        if (isProductAlreadyExists(name)) {
+            List<Product> products = store.getProducts();
+            for (Product product : products) {
+                if (product.getName().equals(name)) {
+                    products.remove(product);
+                    break;
+                }
+            }
+        }
+    }
+
+    public List<Product> getProducts() {
+        return store.getProducts();
+    }
+
+    public Product getProductByName(String name) {
+
+        if (!isValidateProductName(name)) {
+            throw new InvalidProducDataException("Nome do produto não pode ser nulo ou vazio");
+        }
+
+        for (Product product : store.getProducts()) {
+            if (product.getName().equals(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
     public boolean isValidateProductName(String name) {
         if (name == null || name.isBlank()) {
             return false;
