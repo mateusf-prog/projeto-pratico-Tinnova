4793a2f5903ddc1bfc94a8dfd99b18224124e11a
mateusfprog <mateus102006@hotmail.com>
2024-06-16 21:03:12 -0300

    added two new custom exceptions

---
+++ src/service/exceptions/EmptyListException.java
@@ -0,0 +1,8 @@
+package service.exceptions;
+
+public class EmptyListException extends RuntimeException {
+
+        public EmptyListException(String message) {
+            super(message);
+        }
+}
